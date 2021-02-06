1a0500b2d93a827ce28c204a67eaf8924de630c5
github username <d24dust@hotmail.com>
2021-02-06 14:32:14 -0500

    add: user_specs

---
+++ spec/models/model_assc_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe User do
+  user_attr = {name: "John", email: "your@face.com", password: "yourface"}
+  it "creates new User with attributes" do
+    expect(User.new(user_attr)).to be_valid
+  end
+
+  it "has many posts" do
+    u = User.create(user_attr)
+    p = u.posts.create(content: "Lol you get banned?")
+    expect(u.posts.count).to be_equal(1)
+  end
+  it "has the post foreign key assigned" do
+    u = User.create(user_attr)
+    p = u.posts.create(content: "Lol you get banned?")
+    expect(p.user_id).to be_equal(u.id)
+  end
+
+  it "has many comments" do
+    u = User.create(user_attr)
+    p = u.posts.create(content: "Lol you get banned?")
+    c = p.comments.create(content: "i replied to my own post, am lonely", user: u)
+    expect(u.comments.first.id).to be_equal(c.id)
+  end
+
+  it "has many commentented posts through comments" do
+    u = User.create(user_attr)
+    p = u.posts.create(content: "Lol you get banned?")
+    c = p.comments.create(content: "i replied to my own post, am lonely", user: u)
+    expect(u.commented_posts.first.id).to be_equal(p.id)
+  end
+end
