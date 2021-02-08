e0839392a6d7e8963e717e85e7d363a011f3418b
github username <d24dust@hotmail.com>
2021-02-08 12:43:28 -0500

    create: session routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :users, except: [:index] do
     resources :posts, path: "beans", except: [:index]
   end
+
+  get 'login', to: 'sessions#new'
+  post 'logout', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
