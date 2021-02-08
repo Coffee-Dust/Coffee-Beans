f3b66e96b8c4f1770a8cdc10e006eff7339c623f
github username <d24dust@hotmail.com>
2021-02-08 12:23:54 -0500

    feat: basic user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    # raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :home_path
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
