dd4765f864eba363a2f3e1176f25839f754349d7
github username <d24dust@hotmail.com>
2021-02-08 11:35:36 -0500

    add: routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  
+  resources :users, except: [:index] do
+    resources :posts, path: "beans", except: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
