caa310fe23ab87b09b86a9ad80a82c1514593858
github username <d24dust@hotmail.com>
2021-02-08 11:58:24 -0500

    feat: users now have validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :commented_posts, through: :comments, source: :post
+  
   has_secure_password
+
+  validates_presence_of :name, :email, :password
+  validates :password, confirmation: {case_sensitive: true}
+  validates :email, uniqueness: {message: "Email already taken!"}
 end
