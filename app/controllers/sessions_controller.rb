0a3b1716be8dd80b141ca3147d2a1b1b2caaa622
github username <d24dust@hotmail.com>
2021-02-10 13:02:18 -0500

    feat: create new session with omni auth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def new
     @user = User.new
   end
@@ -13,6 +13,17 @@ class SessionsController < ApplicationController
       @user = User.new(email: params[:user][:email])
       flash[:alert] = "Couldn't find that account fewl."
       render :new
+    end
+  end
+
+  def omni_auth_create
+    if request.env['omniauth.auth']
+      @user = User.find_or_create_by_oauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Something went wrong with the third-party login."
+      redirect_to(login_path)
     end
   end
 
