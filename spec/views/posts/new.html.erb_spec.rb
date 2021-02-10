720febcb52ab4048fde8dcfa6fb2524ef9d81625
github username <d24dust@hotmail.com>
2021-02-10 17:33:42 -0500

    add: basic new view specs

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "posts/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  u = User.find_or_create_by(email:"yourface@idk.com") do |user|
+     user.password = "yes"
+     user.name = "Coffee Daddy"
+  end
+  post_content = "Your face is coffee idk"
+  
+  it "creates a new post only if valid" do
+    user_login
+    visit "beans/new"
+    fill_in 'post[content]', with: post_content
+    click_button('Post Bean')
+    visit(user_post_path(u.id, u.posts.first.id))
+    expect(u.posts.first.content).to eq(post_content)
+  end
+
+  it "will display error message if trying to submit invalid post" do
+    user_login
+    visit "beans/new"
+    click_button('Post Bean')
+    expect(page.find("#post_content")[:class].include?("field_with_errors")).to be(true)
+  end
 end
