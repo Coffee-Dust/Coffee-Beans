301195ce506d26495a9e187e3fc58572789c7b5e
github username <d24dust@hotmail.com>
2021-02-10 12:52:38 -0500

    feat: user can be found or created with oauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
   validates :password, confirmation: {case_sensitive: true}
   validates :email, uniqueness: {message: "Email already taken!"}
+
+  def self.find_or_create_by_oauth(oauth_hash)
+    self.find_or_create_by(email: oauth_hash['info']['email']) do |user|
+      user.name = oauth_hash['info']['name']
+      user.password = SecureRandom.hex
+    end
+  end
 end
