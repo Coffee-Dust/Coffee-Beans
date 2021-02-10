33d865fa0195ae996d23395bec8cff76427cce30
github username <d24dust@hotmail.com>
2021-02-10 15:10:48 -0500

    fix: route helper error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to(home_path)
+      redirect_to(root_path)
     else
       @user = User.new(email: params[:user][:email])
       flash[:alert] = "Couldn't find that account fewl."
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
     if request.env['omniauth.auth']
       @user = User.find_or_create_by_oauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to(root_path)
     else
       flash[:alert] = "Something went wrong with the third-party login."
       redirect_to(login_path)
