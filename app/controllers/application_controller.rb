2f2dc05572b219e7d0e906a6c7b034693335a79b
github username <d24dust@hotmail.com>
2021-02-10 17:21:50 -0500

    add: logged_in or redirect checker method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def logged_in_or_redirect
+    redirect_to(login_path) if !logged_in?
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
