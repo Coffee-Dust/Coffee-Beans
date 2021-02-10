62cd3d05c5aadebf05e9499f0151ac2a36eeee29
github username <d24dust@hotmail.com>
2021-02-10 15:19:27 -0500

    config: stopped the warning output

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.silence_get_warning = true
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, "42b4d1eceac90c28c82c", "605da7835b79f54c7c34195fcc51b22dfc71be8c", scope: 'user:email'
