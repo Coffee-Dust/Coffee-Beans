bfda003eafea33ec9fc3fcd956261bcec8f09284
github username <d24dust@hotmail.com>
2021-02-10 15:44:48 -0500

    change: formatting, add: RESTful actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,28 @@
 class PostsController < ApplicationController
+
+  def index
+  end
+
   def show
   end
 
   def new
+
+  end
+
+  def create
+
   end
 
   def edit
+
+  end
+  
+  def update
+
   end
 
-  def index
+  def destroy
+    
   end
 end
