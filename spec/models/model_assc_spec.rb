ca913ca5f4d15558b2c6a297c0c0b11c24ff20ae
github username <d24dust@hotmail.com>
2021-02-10 14:26:27 -0500

    fix: User tests not having uniq emails

--- spec/models/model_assc_spec.rb
+++ spec/models/model_assc_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
 describe User do
-  user_attr = {name: "John", email: "your@face.com", password: "yourface"}
+  user_attr = {name: "John", password: "yourface"}
   it "creates new User with attributes" do
-    expect(User.new(user_attr)).to be_valid
+    expect(User.new({name: "John", email: SecureRandom.hex, password: "yourface"})).to be_valid
   end
 
   it "has many posts" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     p = u.posts.create(content: "Lol you get banned?")
     expect(u.posts.count).to be_equal(1)
   end
   it "has the post foreign key assigned" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     p = u.posts.create(content: "Lol you get banned?")
     expect(p.user_id).to be_equal(u.id)
   end
 
   it "has many comments" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     p = u.posts.create(content: "Lol you get banned?")
     c = p.comments.create(content: "i replied to my own post, am lonely", user: u)
     expect(u.comments.first.id).to be_equal(c.id)
   end
 
   it "has many commentented posts through comments" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     p = u.posts.create(content: "Lol you get banned?")
     c = p.comments.create(content: "i replied to my own post, am lonely", user: u)
     expect(u.commented_posts.first.id).to be_equal(p.id)
   end
 
   it "has a secure password" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     expect(u.password_digest).to_not be_equal("yourface")
   end
 
   it "has a secure password with #authenticate" do
-    u = User.create(user_attr)
+    u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
     expect(u.authenticate(user_attr[:password])).to be_truthy
     expect(u.authenticate("passwordalllowercase")).to be(false)
   end
 end
 
 describe Post do
-  u = User.create({name: "John", email: "your@face.com", password: "yourface"})
+  u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
   p = u.posts.create(content: "Lol idk")
   c1 = p.comments.create(content: "idk", user: u)
 
@@ -64,7 +64,7 @@ describe Post do
 end
 
 describe Comment do
-  u = User.create({name: "John", email: "your@face.com", password: "yourface"})
+  u = User.create({name: "John", email: SecureRandom.hex, password: "yourface"})
   p = u.posts.create(content: "Lol idk")
   c1 = p.comments.create(content: "idk", user: u)
   it "has many reactions" do
