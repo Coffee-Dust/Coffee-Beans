ecb3169629b3477efa23256df4e580d5b829425e
github username <d24dust@hotmail.com>
2021-02-10 19:09:34 -0500

    feat: destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -35,7 +36,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    
+    @post.delete
   end
 
   private
