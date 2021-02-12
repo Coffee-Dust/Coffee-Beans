1e81a22a93e8f5e361c93e03edd64e0a9495505c
github username <d24dust@hotmail.com>
2021-02-12 11:37:29 -0500

    add: model validations

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,6 @@
 class Reaction < ActiveRecord::Base
   belongs_to :reactable, polymorphic: true
   belongs_to :user
+
+  validates_presence_of :reaction_type, :reactable_id, :reaction_type, :user
 end
