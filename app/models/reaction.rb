df671dd8e6eb581ca33b7a144a2437f6bd0350e7
github username <d24dust@hotmail.com>
2021-02-12 14:37:55 -0500

    feat: query methods; add: type table and method

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,4 +3,25 @@ class Reaction < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :reaction_type, :reactable_id, :reaction_type, :user
+
+  # Magic Numbers Definition - ITS MAGIC
+  REACTION_TYPES = {
+    1 => :like,
+    2 => :love,
+    3 => :laugh,
+    4 => :angry,
+    5 => :sad
+  }
+
+  def type
+    REACTION_TYPES[self.reaction_type]
+  end
+
+  def self.find_reactions_for_user_on_reactable(user, reactable)
+    self.where(reactable: reactable).where(user: user)
+  end
+
+  def self.of_type(type)
+    self.find_by(reaction_type: type)
+  end
 end
