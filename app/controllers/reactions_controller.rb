42fefb037ab4df5d839d2f998a9fb8114f0cdfef
github username <d24dust@hotmail.com>
2021-02-12 14:39:47 -0500

    feat: reactions now get removed or created with one request

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,10 +1,20 @@
 class ReactionsController < ApplicationController
   before_action :logged_in_or_redirect
 
+  def react_or_remove
+
+    find_or_build_reaction #assigns @reaction
+
+    if @reaction.persisted?
+      # If it is persisted, then it already exists and must be destroyed!
+      self.destroy
+    else
+      self.create
+    end
+  end
+
   def create
-    # raise params.inspect
-    @reaction = Reaction.new(reaction_params)
-    @reaction.user = current_user
+
     if @reaction.reactable && @reaction.save
       redirect_to_reactable
     else
@@ -13,7 +23,8 @@ class ReactionsController < ApplicationController
   end
 
   def destroy
-    
+    @reaction.destroy
+    redirect_to_reactable
   end
 
   private
@@ -29,6 +40,21 @@ class ReactionsController < ApplicationController
       redirect_to(user_post_path(@reaction.reactable.post.user, @reaction.reactable.post))
     else
       raise "Error in redirect to reactable class; Class invalid: #{@reaction.reactable.class.to_s}"
+    end
+  end
+
+  def find_or_build_reaction
+    new_reaction = Reaction.new(reaction_params)
+    new_reaction.user = current_user
+    users_posts_reactions = Reaction.find_reactions_for_user_on_reactable(current_user, new_reaction.reactable)
+
+    if users_posts_reactions.present? && existing_reaction = users_posts_reactions.of_type(new_reaction.reaction_type)
+      # The reaction already exists. Assigning it to class var 
+      # so i can be killed with fire
+      @reaction = existing_reaction
+    else
+      # Assigning the already built reaction to class var
+      @reaction = new_reaction
     end
   end
 
