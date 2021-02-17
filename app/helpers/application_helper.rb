f4e23bb9fb17698b7e5696c816334e131cb11349
github username <d24dust@hotmail.com>
2021-02-17 12:48:53 -0500

    add: helper method for displaying errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def error_message_for_field(field_name, object)
+    if object.errors.present?
+      return object.errors.full_messages_for(field_name).first
+    else
+      return nil
+    end
+  end
 end
