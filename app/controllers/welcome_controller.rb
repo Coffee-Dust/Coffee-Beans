2a0cf26bce892f8047c009de47c0ebe2e1cb307a
github username <d24dust@hotmail.com>
2021-02-17 15:56:03 -0500

    change: home action, add: profile action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,19 @@
 class WelcomeController < ApplicationController
+
   def home
-    redirect_to(login_path)
+    if logged_in?
+      redirect_to(posts_path)
+    else
+      redirect_to(login_path)
+    end
   end
+
+  def profile
+    if logged_in?
+      redirect_to(user_path(current_user))
+    else
+      redirect_to(login_path)
+    end
+  end
+
 end
