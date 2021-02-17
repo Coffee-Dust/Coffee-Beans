2d0174141ba9ee15af52876b78be85d99c3b4987
github username <d24dust@hotmail.com>
2021-02-17 13:33:31 -0500

    update: methods to handle new #reaction_type string type

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -2,7 +2,7 @@ module ReactionsHelper
 
   def user_has_reacted_with_type?(type, parent)
     @user_reaction_types ||= current_user ? parent.reactions.from_user(current_user).map(&:reaction_type) : []
-    @user_reaction_types.include?(type[0]) ? true : false
+    @user_reaction_types.include?(type) ? true : false
   end
 
 end
