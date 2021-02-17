82250c73de9e4be4edc9e1a091508a2ceb78976e
github username <d24dust@hotmail.com>
2021-02-17 14:22:34 -0500

    fix: comments not getting reacted class when reacted by user

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -1,7 +1,14 @@
 module ReactionsHelper
-
-  def user_has_reacted_with_type?(type, parent)
-    @user_reaction_types ||= current_user ? parent.reactions.from_user(current_user).map(&:reaction_type) : []
+  
+end
+def user_has_reacted_with_type?(type, parent)
+    @last_parent_class ||= parent.class
+    if parent.class == @last_parent_class
+      @user_reaction_types ||= current_user ? parent.reactions.from_user(current_user).map(&:reaction_type) : []
+    else
+      @user_reaction_types = current_user ? parent.reactions.from_user(current_user).map(&:reaction_type) : []
+    end
+    @last_parent_class = parent.class
     @user_reaction_types.include?(type) ? true : false
   end
 
