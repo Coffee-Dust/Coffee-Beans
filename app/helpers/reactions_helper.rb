d90f2f55e57912a81020db3547710aaea17142f0
github username <d24dust@hotmail.com>
2021-02-17 11:11:01 -0500

    add: helper method for partial

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -1,2 +1,8 @@
 module ReactionsHelper
+
+  def user_has_reacted_with_type?(type, parent)
+    @user_reaction_types ||= current_user ? parent.reactions.from_user(current_user).map(&:reaction_type) : []
+    @user_reaction_types.include?(type[0]) ? true : false
+  end
+
 end
