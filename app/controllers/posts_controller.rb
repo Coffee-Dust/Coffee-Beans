78f7417b301df95f34e5c044dea00400a463b6ba
github username <d24dust@hotmail.com>
2021-02-24 14:53:39 -0500

    feat: handles search queries via form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,11 @@ class PostsController < ApplicationController
   before_action :check_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search])
+    else
+      @posts = Post.all
+    end
   end
 
   def show
